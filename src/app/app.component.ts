faccae6400910af3f5977a4472a46a1993500de8
João Pedro <joopedroe936@gmail.com>
2020-04-05 21:41:02 -0300

    Adicionar Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,44 @@
-import { Component } from '@angular/core';
-import {Produto} from './models/produto'
-import {Carrinho} from './models/Carrinho'
-
+import { Component, OnInit } from '@angular/core';
+import {ProdutosService} from './produtos.service'
+import { Produto } from './models/produto';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Loja Virtual';
-  pos=0;
-  pessoa:Produto=new Produto();
+  produtos=null;
+  carrinho=[];
+  constructor(private service:ProdutosService){
 
-  produtos:Produto[]=[{id:1,nome:'Alcool Gel',quant:1,preco:29.99},{id:2,nome:'Mascara descartavel',quant:1,preco:9.99}
-  ];
-
-  carrinho:Carrinho=new Carrinho;
-  
-
-  Adicionar(idE){
-    console.log()
-    var produtoEntrada=this.verificar(idE,this.produtos)
-    console.log(produtoEntrada)
-    if(this.carrinho.produtos.length == 0){
-      this.carrinho.total=0;
-  
-    }
-    var produtoCarrinho=this.verificar(idE,this.carrinho.produtos)
-    var preco=produtoEntrada.preco;
-    if (produtoCarrinho == null){
-      this.carrinho.produtos.push(produtoEntrada);
-    }
-    else{
-      produtoCarrinho.quant+=1
-      console.log(preco);
-      produtoCarrinho.preco=produtoCarrinho.quant*preco;
-    }
-    this.carrinho.total+=preco;
-  
-
-    console.log(this.carrinho)
-    
+  }
+  ngOnInit(): void {
+    this.service.lista().subscribe((dados:any)=>this.produtos=dados);
   }
 
-  verificar(id,lista){
-    for (let pro of lista){
-      if(pro.id ==id){
-        return pro
+  comprar(produto:any){
+    const produtoCarrinho=this.carrinho.find(
+      (p:any)=>p.id === produto.id
+    );
+    if (!produtoCarrinho){
+      this.carrinho.push({
+        id:produto.id,
+        nome:produto.nome,
+        preco:produto.preco,
+        quantidade:1
+      });
       }
+      else{
+        produtoCarrinho.quantidade +=1;
     }
+  }
+  calculaTotalCarrinho(){
+    let total=0;
+    for(const produto of this.carrinho){
+      total+=produto.quantidade*produto.preco;
+    }
+    return total;
   }
 
 }
